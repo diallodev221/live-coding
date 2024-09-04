71a23e995634db07aae4a5d468ee080aedbaa901
Saïfoulaye Diallo <saifoulaye.diallodev@gmail.com>
2024-09-04 18:23:18 +0000

    docs: initial commit

---
+++ src/main/java/com/diallodev/live_coding_cfao_backend/LiveCodingCfaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.diallodev.live_coding_cfao_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCodingCfaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveCodingCfaoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diallodev/live_coding_cfao_backend/LiveCodingCfaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diallodev.live_coding_cfao_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodingCfaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
